ae9592636e78f333b172760cba8aee9f08395485
jegharen@gmail.com <jegharen@gmail.com>
2017-05-05 13:03:18 +0200

    error (intentional

--- sample.c
+++ sample.c
@@ -15,17 +15,19 @@ int addition(int x, int y)
 int mandel(float re, float im)
 {
 	float x = 0, x_new = 0, y = 0;
-    int iter = 0;
+    	int iter = 0;
 	int max = 256;
-	while(iter < max || x*x+y*y >= 4) {
+        while(iter < max && x*x+y*y <= 4) {
 		x_new = x*x - y*y + re;
 		y = 2*x*y + im;
 		x = x_new;
 		iter++;
 		//printf("iteration %d\n", iter);
 	}
+
 	//printf("iteration %d\n", iter);
-	if(iter >= max) {
+	
+	if(iter == max) {
 		return 1;
 	} else {
 		return 0;
